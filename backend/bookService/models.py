e7f6406faf7d53cdba89b25a46b3ddfeb33c6257
koziolek24 <kozlowski11maciej@gmail.com>
2025-03-11 18:57:41 +0100

    changed id to autofield; created Categories table

--- backend/bookService/models.py
+++ backend/bookService/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 class Users(models.Model):
-    id = models.IntegerField(primary_key=True)
-    username = models.CharField(max_length=100, null=True)  # Include any fields you want
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True, null=False, default="")
+    created_at = models.DateField(auto_now_add=True)
+
+class Categories(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False, unique=True, default="")
+
