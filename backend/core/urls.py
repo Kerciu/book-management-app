e0edbdfb69795afd4c5a0259f1fc70d06f656cc8
koziolek24 <kozlowski11maciej@gmail.com>
2025-03-16 09:23:32 +0100

    forgot in last commit to add this file

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Add your API URLs here
-    # path('api/', include('api.urls')),
+    path('api/', include('bookService.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
