6e602daf371c54e15df73ffb474cf5ef68e4ba10
Kerciu <kerciuuu@gmail.com>
2025-03-16 18:14:34 +0100

    added email validator method to user manager that raises error when email is invalid

--- backend/authentication/managers.py
+++ backend/authentication/managers.py
@@ -1,4 +1,22 @@
 from django.contrib.auth.models import BaseUserManager
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
 
 class UserManager(BaseUserManager):
-    pass
+
+    def email_validator(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError("Please enter a valid email address")
+
+    def create_user(
+        self,
+        email,
+        first_name,
+        last_name,
+        password,
+        **other_fields
+    ):
+        pass
