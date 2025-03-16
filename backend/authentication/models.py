dbac8fcdf3c8ca690216150462153e91255606a9
Kerciu <kerciuuu@gmail.com>
2025-03-16 18:08:01 +0100

    created custom user model inheriting from abstract user class

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomBaseUser(AbstractUser):
+
+    email = models.EmailField(unique=True, max_length=255)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'password']
+
+    # objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
