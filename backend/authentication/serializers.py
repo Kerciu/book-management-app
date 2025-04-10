57ff3cfe2df16a303d896dc346fc62462e3fd131
Kerciu <Kerciuuu@gmail.com>
2025-04-10 23:24:34 +0200

    created user register serializer used for registering users

---
+++ backend/authentication/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    re_password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name', 'password', 're_password']
