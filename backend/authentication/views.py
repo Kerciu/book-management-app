733cec7b8c7546f1b7d60422e8147cf05d39e4d0
Kerciu <Kerciuuu@gmail.com>
2025-04-10 23:27:52 +0200

    creater register class based view

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserRegisterSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+
+class UserRegisterView(GenericAPIView):
+    serializer_class = UserRegisterSerializer
