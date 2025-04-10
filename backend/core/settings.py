194fde2b02edf8d7258acffe9d06de3470a3ce74
Kerciu <Kerciuuu@gmail.com>
2025-04-11 00:05:16 +0200

    added email configuration to the core settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -122,3 +122,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
+
+EMAIL_HOST_USER = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
