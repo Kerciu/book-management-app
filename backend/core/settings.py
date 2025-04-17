b6bc0216379121e468bc61e07c403291e351eff0
Kerciu <Kerciuuu@gmail.com>
2025-04-18 00:54:15 +0200

    added simple jwt settings for token lifetimes and auth header type

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import timedelta
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
 EMAIL_HOST_USER = os.getenv('EMAIL_BACKEND')
