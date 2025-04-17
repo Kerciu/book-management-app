b2925f4e1ad634326d25c94bf7f6dd0f552f4abe
Kerciu <Kerciuuu@gmail.com>
2025-04-18 00:09:52 +0200

    created endpoint to verify user's email by their provided OTP's

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, ValidateRegisterView
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name="register")
+    path('register/', UserRegisterView.as_view(), name="register"),
+    path('verify-user/', ValidateRegisterView.as_view(), name="verify")
 ]
