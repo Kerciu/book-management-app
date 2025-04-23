dabde746ab934d78770c2b92f2565f187c0d8789
Kerciu <Kerciuuu@gmail.com>
2025-04-24 00:18:35 +0200

    created test case for email serializer, set up the user object model

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import CustomUser
+from .serializers import ResendEmailSerializer
 
 # Create your tests here.
+
+class ResendEmailSerializerTest(TestCase):
+
+    user = CustomUser.objects.create_user(
+        username="test123",
+        email="kacper@gmail.com",
+        first_name='Test',
+        last_name='User',
+        password="2137abcAA!"
+    )
+
+    
