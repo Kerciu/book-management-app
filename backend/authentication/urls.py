b5811474315774d26ef6c6f2613a6906942c3235
Kerciu <Kerciuuu@gmail.com>
2025-04-24 00:15:44 +0200

    added resend email endpoint to the app urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import LoginUserView, UserRegisterView, ValidateRegisterView
+from .views import (
+    LoginUserView,
+    UserRegisterView,
+    ValidateRegisterView,
+    ResendEmailView
+)
 
 urlpatterns = [
     path('login/', LoginUserView.as_view(), name="login"),
     path('register/', UserRegisterView.as_view(), name="register"),
     path('verify-user/', ValidateRegisterView.as_view(), name="verify"),
+    path('resend-email/', ResendEmailView.as_view(), name='resend'),
 ]
