f531098bb7046ef1ae5d001d91f27833c4cc10b3
Kerciu <Kerciuuu@gmail.com>
2025-04-24 00:26:04 +0200

    moved test cases into their separate folders

---
+++ backend/authentication/tests/serializer_tests/tests.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from ...models import CustomUser
+from ...serializers import ResendEmailSerializer
+from django.core.exceptions import ValidationError
+
+# Create your tests here.
+
+class ResendEmailSerializerTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="test123",
+            email="kacper@gmail.com",
+            first_name='Test',
+            last_name='User',
+            password="2137abcAA!"
+        )
+
+    def test_valid_email(self):
+        serializer = ResendEmailSerializer.create(data={ "email": "kerciu@gmail.com"})
+        self.assertTrue(serializer.is_valid())
+    
+    def test_invalid_email(self):
+        serializer = ResendEmailSerializer.create(data={ "email": "this is invalid for sure"})
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('email', serializer.errors)
+
+    def test_nonexistent_email(self):
+        serializer = ResendEmailSerializer(data={'email': 'nonexistent@example.com'})
+        with self.assertRaises(ValidationError):
+            serializer.is_valid(raise_exception=True)
