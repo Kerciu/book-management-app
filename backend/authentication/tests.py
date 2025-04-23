2e59b95f1c0c52b0c35b76e7a55a54d9a01466b0
Kerciu <Kerciuuu@gmail.com>
2025-04-24 00:22:40 +0200

    added invalid email and nonexistend email searching test cases for resend email serializer

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import CustomUser
 from .serializers import ResendEmailSerializer
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
 
@@ -18,3 +19,12 @@ class ResendEmailSerializerTest(TestCase):
         serializer = ResendEmailSerializer.create(data={ "email": "kerciu@gmail.com"})
         self.assertTrue(serializer.is_valid())
     
+    def test_invalid_email(self):
+        serializer = ResendEmailSerializer.create(data={ "email": "this is invalid for sure"})
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('email', serializer.errors)
+
+    def test_nonexistent_email(self):
+        serializer = ResendEmailSerializer(data={'email': 'nonexistent@example.com'})
+        with self.assertRaises(ValidationError):
+            serializer.is_valid(raise_exception=True)
