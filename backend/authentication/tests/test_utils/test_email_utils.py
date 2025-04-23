a5ad5f5e3b9a546ac24b73e2b4ac3961f8ea534e
Kerciu <Kerciuuu@gmail.com>
2025-04-24 00:38:58 +0200

    added test for utils module to check otp validity and mock email sending call

--- backend/authentication/tests/test_utils/test_email_utils.py
+++ backend/authentication/tests/test_utils/test_email_utils.py
@@ -1,7 +1,30 @@
+from unittest.mock import patch
 from django.test import TestCase
-from ...utils import generate_otp
+from ...models import CustomUser, OneTimePassword
+from ...utils import generate_otp, send_code_to_user
 
 class EmailUtilsTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email='test@example.com',
+            first_name="kacper",
+            last_name="gorski",
+            username='testuser',
+            password='testpass123'
+        )
+    
     def test_generate_otp_length(self):
         otp = generate_otp()
         self.assertEqual(len(otp), 6)
+
+    def test_generate_otp_digits(self):
+        otp = generate_otp()
+        self.assertTrue(otp.isdigit())
+
+    @patch('authentication.utils.EmailMessage')
+    def test_send_code_to_user(self, mock_email):
+        send_code_to_user(self.user.email)
+        
+        self.assertEqual(OneTimePassword.objects.count(), 1)
+        
+        mock_email.assert_called_once()
