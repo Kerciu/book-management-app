1c2b6ba9564c5c277e149c36d728fdd63daa5b97
Kerciu <Kerciuuu@gmail.com>
2025-04-24 00:19:30 +0200

    fix: moved test case set up into the setUp built-in method

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -5,13 +5,15 @@ from .serializers import ResendEmailSerializer
 # Create your tests here.
 
 class ResendEmailSerializerTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="test123",
+            email="kacper@gmail.com",
+            first_name='Test',
+            last_name='User',
+            password="2137abcAA!"
+        )
 
-    user = CustomUser.objects.create_user(
-        username="test123",
-        email="kacper@gmail.com",
-        first_name='Test',
-        last_name='User',
-        password="2137abcAA!"
-    )
-
+    def test_is_valid_email(self):
+        pass
     
