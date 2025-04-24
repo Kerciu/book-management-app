32590f7a1c0312f1c4aeaeaf79ebb743046c893b
Kerciu <Kerciuuu@gmail.com>
2025-04-24 19:01:20 +0200

    test: set up password reset test for serializer

---
+++ backend/authentication/tests/test_serializers/test_password_reset.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from ...models import CustomUser
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from unittest.mock import patch
+
+CustomUser = get_user_model()
+
+
+class PasswordResetTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = CustomUser.objects.create_user(
+            email='test@example.com',
+            password='testpass123',
+            first_name='Test'
+        )
+        self.url = reverse('password-reset')
