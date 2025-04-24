7d6cf4d97929b939c1d8e5e844f699fab1c4ebf3
Kerciu <Kerciuuu@gmail.com>
2025-04-24 19:57:06 +0200

    changed naming of url patterns, added password reset endpoint to the urls file

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,12 +3,14 @@ from .views import (
     LoginUserView,
     UserRegisterView,
     ValidateRegisterView,
-    ResendEmailView
+    ResendEmailView,
+    PasswordResetView,
 )
 
 urlpatterns = [
     path('login/', LoginUserView.as_view(), name="login"),
     path('register/', UserRegisterView.as_view(), name="register"),
-    path('verify-user/', ValidateRegisterView.as_view(), name="verify"),
-    path('resend-email/', ResendEmailView.as_view(), name='resend'),
+    path('verify-user/', ValidateRegisterView.as_view(), name="verify-user"),
+    path('resend-email/', ResendEmailView.as_view(), name='resend-email'),
+    path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
 ]
