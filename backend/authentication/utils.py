5874ba10ba91348d5d09e9876db2ff731d69fd0e
Kerciu <Kerciuuu@gmail.com>
2025-04-24 18:45:23 +0200

    created function that sends password reset email to the user

--- backend/authentication/utils.py
+++ backend/authentication/utils.py
@@ -5,6 +5,9 @@ import random
 from django.core.mail import EmailMessage
 from django.conf import settings
 from .models import CustomUser, OneTimePassword
+from django.core.mail import send_mail
+from django.contrib.sites.models import Site
+from django.urls import reverse
 
 def generate_otp():
     return "".join(([str(random.randint(0, 9)) for i in range(6)]))
@@ -42,3 +45,21 @@ def generate_password_reset_tokens(user):
     token = PasswordResetTokenGenerator().make_token(uid)
 
     return uid, token
+
+def send_password_reset_email(user, uid, token, request):
+    site_domain = Site.objects.get_current(request).domain
+    relative_link = reverse('confirm-password-reset', kwargs={'uid': uid, 'token': token})
+    abs_link = f'http://{site_domain}{relative_link}'
+    
+    subject = "Password Reset Request"
+    message = f"Hello {user.first_name.capitalize()}! Use this link to reset your password:\n{abs_link}"
+    from_email = settings.DEFAULT_FROM_EMAIL
+   
+    send_email = EmailMessage(
+        subject=subject,
+        body=message,
+        from_email=from_email,
+        to=[user.email]
+    )
+
+    send_email.send(fail_silently=False)
