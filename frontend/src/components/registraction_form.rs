b30752696db037fbb105281ef0de62f164fef9d6
Filip Kopyt <filipkopyt14@gmail.com>
2025-05-07 05:41:06 +0200

    Fixed registration form: refactoring, missing field

--- frontend/src/components/registraction_form.rs
+++ frontend/src/components/registraction_form.rs
@@ -8,6 +8,7 @@ use super::send_post_request;
 #[derive(Serialize, Clone, Copy, Default)]
 /// contains items necesary to send request
 struct RegisterRequest {
+    username: RwSignal<String>,
     email: RwSignal<String>,
     first_name: RwSignal<String>,
     last_name: RwSignal<String>,
@@ -18,6 +19,7 @@ struct RegisterRequest {
 #[derive(Debug, Clone)]
 /// Errors that may be returned from request
 enum ValidationError {
+    Username(String),
     Email(String),
     Password(String),
     RePassword(String),
@@ -51,10 +53,17 @@ async fn post(data: RegisterRequest) -> anyhow::Result<RegisterResponse> {
         let res = res.as_object().ok_or(anyhow!("Empty Response"))?;
         let mut ret = Vec::new();
 
-        if res
-            .iter()
-            .any(|(key, _)| !["email", "password", "re_password"].contains(&key.as_str()))
-        {
+        if res.iter().any(|(key, _)| {
+            ![
+                "email",
+                "password",
+                "re_password",
+                "username",
+                "first_name",
+                "last_name",
+            ]
+            .contains(&key.as_str())
+        }) {
             return Err(anyhow!(
                 "Invalid resposce: unexpected keys. Content:\n{res:?}"
             ));
@@ -70,11 +79,14 @@ async fn post(data: RegisterRequest) -> anyhow::Result<RegisterResponse> {
                     .ok_or(anyhow!("Exepected String, got {:?}", value))?
                     .to_string();
                 ret.push(match key.as_str() {
-                    "email" => ValidationError::Email(value),
-                    "password" => ValidationError::Password(value),
-                    "re_password" => ValidationError::RePassword(value),
+                    "email" => ValidationError::Email,
+                    "password" => ValidationError::Password,
+                    "re_password" => ValidationError::RePassword,
+                    "username" => ValidationError::Username,
+                    "first_name" => ValidationError::FirstName,
+                    "last_name" => ValidationError::LastName,
                     _ => unreachable!(),
-                });
+                }(value));
             }
         }
 
@@ -96,8 +108,8 @@ pub fn registraction_form() -> impl IntoView {
             return RegisterResponse::Waiting;
         }
 
-        let t = &*send_request.value().read();
-        match t {
+        let maybe_result = &*send_request.value().read();
+        match maybe_result {
             Some(result) => match result {
                 Ok(response) => response.clone(),
                 Err(err) => {
@@ -110,15 +122,33 @@ pub fn registraction_form() -> impl IntoView {
     };
 
     let response_display = move || match response() {
-        RegisterResponse::Ok => "Registration Successful",
+        RegisterResponse::Ok => "Registration successful",
         RegisterResponse::NoResponse => "Something went wrong, try again",
         RegisterResponse::NoRequest => "",
         RegisterResponse::Waiting => "Waiting for server",
         RegisterResponse::Err(_) => "Registration failed",
     };
 
-    let form_email = move || {
-        view! {
+    // TODO: Can we write a macro to not repeat the form's fields?
+    //       Should we?
+    view! {
+        <form>
+            <div>
+                <label>"Username"</label>
+                <input type="text" bind:value=request.username required />
+                <p>
+                {move ||
+                    if let RegisterResponse::Err(arr) = response()
+                    && let Some(ValidationError::Username(error)) = arr
+                        .iter()
+                        .find(|i| matches!(i, ValidationError::Username(_))) {
+                        error.clone()
+                    } else {
+                        "".to_string()
+                    }
+                }
+                </p>
+            </div>
             <div>
                 <label>"E-mail"</label>
                 <input type="email" bind:value=request.email required />
@@ -135,11 +165,38 @@ pub fn registraction_form() -> impl IntoView {
                 }
                 </p>
             </div>
-        }
-    };
-
-    let form_password = move || {
-        view! {
+            <div>
+                <label>"First Name"</label>
+                <input type="text" bind:value=request.first_name required />
+                <p>
+                {move ||
+                    if let RegisterResponse::Err(arr) = response()
+                    && let Some(ValidationError::FirstName(error)) = arr
+                        .iter()
+                        .find(|i| matches!(i, ValidationError::FirstName(_))) {
+                        error.clone()
+                    } else {
+                        "".to_string()
+                    }
+                }
+                </p>
+            </div>
+            <div>
+                <label>"Last Name"</label>
+                <input type="text" bind:value=request.last_name required />
+                <p>
+                {move ||
+                    if let RegisterResponse::Err(arr) = response()
+                    && let Some(ValidationError::LastName(error)) = arr
+                        .iter()
+                        .find(|i| matches!(i, ValidationError::LastName(_))) {
+                        error.clone()
+                    } else {
+                        "".to_string()
+                    }
+                }
+                </p>
+            </div>
             <div>
                 <label>"Password"</label>
                 <input type="password" bind:value=request.password required />
@@ -156,11 +213,6 @@ pub fn registraction_form() -> impl IntoView {
                 }
                 </p>
             </div>
-        }
-    };
-
-    let form_repassword = move || {
-        view! {
             <div>
                 <label>"Repeat Password"</label>
                 <input type="password" bind:value=request.re_password required />
@@ -177,29 +229,12 @@ pub fn registraction_form() -> impl IntoView {
                 }
                 </p>
             </div>
-        }
-    };
-
-    view! {
-        <form>
-            { form_email }
-            <div>
-                <label>"First Name"</label>
-                <input type="text" bind:value=request.first_name required />
-            </div>
-            <div>
-                <label>"Last Name"</label>
-                <input type="text" bind:value=request.last_name required />
-            </div>
-            { form_password }
-            { form_repassword }
-
             <input type="button" on:click=move |_| {
                 send_request.dispatch(request);
             } value="Register" />
         </form>
         <p>
-        {response_display}
+            {response_display}
         </p>
     }
 }
