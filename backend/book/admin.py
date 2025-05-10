9cdbc1b0a4531557d1b536f02c20f6f073e24532
Kerciu <kerciuuu@gmail.com>
2025-05-10 23:32:44 +0200

    feat: add Author and Publisher models with relationships to Book

--- backend/book/admin.py
+++ backend/book/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from .models import (
+    Author,
+    Publisher,
+    Book,
+)
 
 # Register your models here.
 
+admin.site.register(Author)
+admin.site.register(Publisher)
 admin.site.register(Book)
--- backend/book/models.py
+++ backend/book/models.py
@@ -3,5 +3,32 @@ from django.db import models
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
-    pass
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    authors = models.ManyToManyField(Author)
+
+    isbn = models.CharField(max_length=13, unique=True)
+    published_at = models.DateField(null=True, blank=True)
+    publishers = models.ManyToManyField(Publisher)
+
+    def __str__(self):
+        return self.title
