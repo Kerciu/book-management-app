a332c587b61601dd3a207fd8219b1ec03e9743ed
Kerciu <kerciuuu@gmail.com>
2025-05-11 15:04:19 +0200

    feat: enhance book serializer  to handle nested authors, publishers, and genres during creation

--- backend/book/serializers.py
+++ backend/book/serializers.py
@@ -22,6 +22,32 @@ class PublisherSerializer(serializers.Serializer):
 
 
 class BookSerializer(serializers.Serializer):
+    authors = AuthorSerializer(many=True, required=False)
+    publishers = PublisherSerializer(many=True, required=False)
+    genres = GenreSerializer(many=True, required=False)
+
     class Meta:
         model = Book
         fields = "__all__"
+
+    def create(self, validated_data):
+
+        authors_data = validated_data.pop("authors", [])
+        publishers_data = validated_data.pop("publishers", [])
+        genres_data = validated_data.pop("genres", [])
+
+        book = Book.objects.create(**validated_data)
+
+        for author in authors_data:
+            if "id" in author:
+                book.authors.add(author["id"])
+
+        for publisher in publishers_data:
+            if "id" in publisher:
+                book.publishers.add(publisher["id"])
+
+        for genre in genres_data:
+            if "id" in genre:
+                book.genres.add(genre["id"])
+
+        return book
