571754fd91cf2cd204f7b96a8e6e99175abd5e85
Kerciu <kerciuuu@gmail.com>
2025-05-11 15:29:29 +0200

    refactor: implement custom permission class for viewsets and update permission handling

--- backend/book/views.py
+++ backend/book/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from .models import Book, Author, Publisher, Genre
 from .serializers import (
@@ -12,25 +12,37 @@ from .serializers import (
 # Create your views here.
 
 
-class BookViewSet(viewsets.ViewSet):
+class IsAdminOrReadOnly(IsAuthenticated):
+    def has_permission(self, request, view):
+        if request.method in ["GET", "HEAD", "OPTIONS"]:
+            return super().has_permission(request, view)
+
+        return request.user.is_staff or request.user.is_superuser
+
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = IsAuthenticated
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
 
 
-class AuthorViewSet(viewsets.ViewSet):
+class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    permission_classes = IsAuthenticated
+    permission_classes = [IsAdminOrReadOnly]
 
 
-class PublisherViewSet(viewsets.ViewSet):
+class PublisherViewSet(viewsets.ModelViewSet):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
-    permission_classes = IsAuthenticated
+    permission_classes = [IsAdminOrReadOnly]
 
 
-class GenreViewSet(viewsets.ViewSet):
+class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = IsAuthenticated
+    permission_classes = [IsAdminOrReadOnly]
