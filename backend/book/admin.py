b195e72990cd7f384b3899f9865eaa1f892d8b29
Kerciu <kerciuuu@gmail.com>
2025-05-11 15:54:17 +0200

    fix: ensure Genre model is registered in the admin site

--- backend/book/admin.py
+++ backend/book/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from .models import (
-    Author,
-    Publisher,
-    Book,
-)
+from .models import Author, Publisher, Book, Genre
 
 # Register your models here.
 
 admin.site.register(Author)
 admin.site.register(Publisher)
 admin.site.register(Book)
+admin.site.register(Genre)
