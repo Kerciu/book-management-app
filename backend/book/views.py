24132991700936eb928a05c536c928ae799dc306
Kerciu <kerciuuu@gmail.com>
2025-05-11 15:11:28 +0200

    implemented viewsets for Book, Author, Publisher, and Genre models with authentication

--- backend/book/views.py
+++ backend/book/views.py
@@ -1,7 +1,36 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Book, Author, Publisher, Genre
+from .serializers import (
+    BookSerializer,
+    AuthorSerializer,
+    PublisherSerializer,
+    GenreSerializer,
+)
 
 # Create your views here.
 
 
 class BookViewSet(viewsets.ViewSet):
-    pass
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = IsAuthenticated
+
+
+class AuthorViewSet(viewsets.ViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = IsAuthenticated
+
+
+class PublisherViewSet(viewsets.ViewSet):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+    permission_classes = IsAuthenticated
+
+
+class GenreViewSet(viewsets.ViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = IsAuthenticated
