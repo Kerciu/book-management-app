01cbfe69e2e16df884ff6457c84257fa339814c0
Kerciu <kerciuuu@gmail.com>
2025-05-11 18:19:50 +0200

    optimized models with database indexes

--- backend/book/models.py
+++ backend/book/models.py
@@ -7,11 +7,11 @@ from django.core.validators import MinLengthValidator
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, db_index=True)
 
     bio = models.TextField(blank=True)
 
-    birth_date = models.DateField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True, db_index=True)
     death_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
@@ -19,9 +19,9 @@ class Author(models.Model):
 
 
 class Publisher(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True, db_index=True)
 
-    website = models.URLField(blank=True)
+    website = models.URLField(blank=True, unique=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
@@ -29,14 +29,14 @@ class Publisher(models.Model):
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True, db_index=True)
 
     def __str__(self):
         return self.name
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField(blank=True)
     authors = models.ManyToManyField(Author)
 
@@ -46,7 +46,7 @@ class Book(models.Model):
         max_length=13, unique=True, validators=[MinLengthValidator(10)]
     )
 
-    published_at = models.DateField(null=True, blank=True)
+    published_at = models.DateField(null=True, blank=True, db_index=True)
     publishers = models.ManyToManyField(Publisher)
 
     page_count = models.PositiveIntegerField(null=True, blank=True)
