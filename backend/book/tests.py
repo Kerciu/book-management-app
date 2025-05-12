abddce99522713727fbb6979503176471217aaef
Kerciu <kerciuuu@gmail.com>
2025-05-12 14:19:22 +0200

    test: added tests for AuthorSerializer to validate author data and handle missing fields

--- backend/book/tests.py
+++ backend/book/tests.py
@@ -1,10 +1,42 @@
 from django.test import TestCase
+from rest_framework.serializers import ValidationError
+
+from .serializers import AuthorSerializer
 
 # Create your tests here.
 
 
 class AuthorSerializerTest(TestCase):
-    pass
+    def setUp(self):
+        self.valid_data = {
+            "first_name": "George",
+            "last_name": "Orwell",
+            "birth_date": "1906-06-25",
+            "death_date": "1950-01-21",
+        }
+
+    def test_valid_author(self):
+        serializer = AuthorSerializer(data=self.valid_data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_invalid_death_date_before_birth(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data["death_date"] = "1905-06-25"
+        serializer = AuthorSerializer(data=invalid_data)
+
+        with self.assertRaises(ValidationError) as context:
+            serializer.is_valid(raise_exception=True)
+
+        self.assertIn("Death date must be after birth date", str(context.exception))
+
+    def test_valid_missing_fields(self):
+        copied_data = self.valid_data.copy()
+
+        del copied_data["birth_date"]
+        del copied_data["death_date"]
+
+        serializer = AuthorSerializer(data=copied_data)
+        self.assertTrue(serializer.is_valid())
 
 
 class PublisherSerializerTest(TestCase):
