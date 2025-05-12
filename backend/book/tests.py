ceeea5bf66eb4a6a654dc895c3406f02d40d1bb9
Kerciu <kerciuuu@gmail.com>
2025-05-12 14:10:38 +0200

    created empty test cases classes for serializers and viewsets

--- backend/book/tests.py
+++ backend/book/tests.py
@@ -3,5 +3,33 @@ from django.test import TestCase
 # Create your tests here.
 
 
-class BookTest(TestCase):
+class AuthorSerializerTest(TestCase):
+    pass
+
+
+class PublisherSerializerTest(TestCase):
+    pass
+
+
+class GenreSerializerTest(TestCase):
+    pass
+
+
+class BookSerializerTest(TestCase):
+    pass
+
+
+class BookViewSetTest(TestCase):
+    pass
+
+
+class AuthorViewSetTest(TestCase):
+    pass
+
+
+class PublisherViewSetTest(TestCase):
+    pass
+
+
+class GenreViewSetTest(TestCase):
     pass
