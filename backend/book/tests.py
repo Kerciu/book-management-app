dce7b18a7aaf55b65a5982f46a82805eb575b087
Kerciu <kerciuuu@gmail.com>
2025-05-12 14:32:20 +0200

    test: add genre serializer tests for validation of genre name

--- backend/book/tests.py
+++ backend/book/tests.py
@@ -2,7 +2,11 @@ from django.test import TestCase
 from rest_framework.serializers import ValidationError
 from rest_framework.exceptions import ValidationError as URLValidationError
 
-from .serializers import AuthorSerializer, PublisherSerializer
+from .serializers import (
+    AuthorSerializer,
+    PublisherSerializer,
+    GenreSerializer,
+)
 
 # Create your tests here.
 
@@ -66,7 +70,20 @@ class PublisherSerializerTest(TestCase):
 
 
 class GenreSerializerTest(TestCase):
-    pass
+    def setUp(self):
+        self.valid_data = {"name": "Warsaw University of Technology"}
+        self.invalid_data = {"name": ""}
+
+    def test_valid_genre(self):
+        serializer = GenreSerializer(data=self.valid_data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_genre_missing_required_name(self):
+        serializer = GenreSerializer(data=self.invalid_data)
+        with self.assertRaises(ValidationError) as context:
+            serializer.is_valid(raise_exception=True)
+
+        self.assertIn("This field may not be blank.", str(context.exception))
 
 
 class BookSerializerTest(TestCase):
