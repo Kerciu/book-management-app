c3dbd3487b6e93b496c7561b969236ece34cb82b
Kerciu <kerciuuu@gmail.com>
2025-05-14 14:46:29 +0200

    deleted otp from registered models

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import CustomUser, OneTimePassword
+from .models import CustomUser
 
 # Register your models here.
 
 admin.site.register(CustomUser)
-admin.site.register(OneTimePassword)
