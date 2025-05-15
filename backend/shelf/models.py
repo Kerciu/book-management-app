8a1e24881c326b90312b63e467bdb41df992beac
Julia Czosnek <julia.cz13@o2.pl>
2025-05-15 14:26:59 +0200

    finished Shelf model

--- backend/shelf/models.py
+++ backend/shelf/models.py
@@ -9,15 +9,19 @@ class Shelf(models.Model):
     SHELF_TYPES = (
         ('want_to_read', 'Want to Read'),
         ('currently_reading', 'Currently Reading'),
-        ('read', 'Read')
+        ('read', 'Read'),
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="shelves")
     name = models.CharField(
         max_length=30,
         validators=[MinLengthValidator(5)]
     )
+    is_default = models.BooleanField(default=False)
+    shelf_type = models.CharField(max_length=20, choices=SHELF_TYPES, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name} (by {self.user.username})"
+
+
