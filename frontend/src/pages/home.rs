5d78ae8a0e08c38b649825aba9c752ceb71dd771
Szymon Kaminski <szymon.kaminkk@gmail.com>
2025-05-15 20:59:51 +0200

    refactor: organized styles.css styles names and order

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -5,11 +5,11 @@ pub fn HomePage() -> impl IntoView {
     view! {
                 <main>
                     <div class="home-page-welcome-text" >"Witaj w BookUp"</div>
-                    <div class="container">
-                        <div class="welcome-info">"Zacznij korzystać z BookUp"</div>
-                        <div class="welcome-info-bold">"BookUp to innowacyjna aplikacja do zarządzania kolekcją książek, która umożliwia dodawanie, ocenianie, recenzowanie i dzielenie się książkami z przyjaciółmi"</div>
-                        <div class="welcome-info">"Dołącz już dziś!"</div>
-                        <button class="button-start">"Dołącz teraz"</button>
+                    <div class="container-flex">
+                        <div class="text-lines">"Zacznij korzystać z BookUp"</div>
+                        <div class="text-lines-bold">"BookUp to innowacyjna aplikacja do zarządzania kolekcją książek, która umożliwia dodawanie, ocenianie, recenzowanie i dzielenie się książkami z przyjaciółmi"</div>
+                        <div class="text-lines">"Dołącz już dziś!"</div>
+                        <button class="button-pop">"Dołącz teraz"</button>
                     </div>
                     <div style="display: flex; gap: 0px; align-items: center; margin-left: 20px; margin-right: 20px; margin-top: 80px;">
                         <img src="https://static.wixstatic.com/media/117514_dc6b7bd47a3e42df8ab688dd92fe65c5~mv2.png" style="width: 25vw; height: 316px; object-fit: cover;"></img>
@@ -17,21 +17,21 @@ pub fn HomePage() -> impl IntoView {
                     </div>
                     <div class="white-section">
                         <div style="height:60px"></div>
-                        <div class="content-container">
+                        <div class="container-flex">
                             <div class="text-side">
-                                <div class="information-title-text">"Informacje"</div>
-                                <div class="information-body-text">"BookUp to aplikacja stworzona z myślą o pasjonatach książek. Nasza platforma umożliwia łatwe dodawanie książek do własnej kolekcji, ich ocenę, pisanie recenzji oraz udostępnianie rekomendacji z przyjaciółmi. Dzięki BookUp możesz odkrywać nowe tytuły i dzielić się swoimi ulubionymi lekturami z innymi."</div>
-                                <button class="button-more">"Dowiedź się więcej"</button>
+                                <div class="title-text-home-page">"Informacje"</div>
+                                <div class="body-text">"BookUp to aplikacja stworzona z myślą o pasjonatach książek. Nasza platforma umożliwia łatwe dodawanie książek do własnej kolekcji, ich ocenę, pisanie recenzji oraz udostępnianie rekomendacji z przyjaciółmi. Dzięki BookUp możesz odkrywać nowe tytuły i dzielić się swoimi ulubionymi lekturami z innymi."</div>
+                                <button class="button-pop-reverse">"Dowiedź się więcej"</button>
                             </div>
                             
                             <img src="https://static.wixstatic.com/media/117514_947e901d593448e98241dfe06415d791~mv2.png" alt="Description" class="image-side"></img>
                         </div>
                         <div class="text-title">"Nasze usługi"</div>
-                        <div class="container">
+                        <div class="container-flex">
                             <div>
                               <img ssrc="https://static.wixstatic.com/media/117514_2210c99136a548e882210b1319375561~mv2.png" style="width: auto; height: 295px; object-fit: cover;"></img>
-                              <div class="text-title">"Dodawanie Książek"</div>
-                              <div class="information-body-text">"Dodawaj swoje ulubione książki do personalnej kolekcji w BookUp i miej do nich zawsze łatwy dostęp."</div>
+                              <div class="title-text-home-page">"Dodawanie Książek"</div>
+                              <div class="body-text">"Dodawaj swoje ulubione książki do personalnej kolekcji w BookUp i miej do nich zawsze łatwy dostęp."</div>
                             </div>
                         </div>
 
