d3f9e4c43b7ce25a32ab4d871d2adddef7dfdf7e
Kerciu <kerciuuu@gmail.com>
2025-05-15 11:58:53 +0200

    created initial Review and BookRating models with validation

--- backend/review/models.py
+++ backend/review/models.py
@@ -1,3 +1,28 @@
-# from django.db import models
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth import get_user_model
+from ..book.models import Book
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    rating = models.PositiveIntegerField(
+        MinValueValidator(1),
+        MaxValueValidator(5),
+    )
+
+
+class BookRating(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    average_rating = models.FloatField(
+        MinValueValidator(1),
+        MaxValueValidator(5),
+    )
+
+    num_reviews = models.PositiveIntegerField()
