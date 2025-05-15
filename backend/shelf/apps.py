9b12eadeaf23167d565930ecf19c2e103236cdbc
Julia Czosnek <julia.cz13@o2.pl>
2025-05-15 20:34:02 +0200

    added signal to shelf

--- backend/shelf/apps.py
+++ backend/shelf/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ShelfConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "shelf"
+
+    def ready(self):
+        import shelf.signals
