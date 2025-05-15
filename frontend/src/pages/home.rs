e0785ce225173e37d8975cefbca9ae161d2fc6a5
Szymon Kaminski <szymon.kaminkk@gmail.com>
2025-05-15 21:37:32 +0200

    feat: finished home page html baseline

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -4,6 +4,10 @@ use leptos::html::*;
 pub fn HomePage() -> impl IntoView {
     view! {
                 <main>
+                    <header>
+                        <div class="spacer"></div>
+                        <button class="button-login" style="margin-top: 0px;">"Zaloguj się"</button>
+                    </header>
                     <div class="home-page-welcome-text" >"Witaj w BookUp"</div>
                     <div class="container-flex">
                         <div class="text-lines">"Zacznij korzystać z BookUp"</div>
@@ -21,7 +25,7 @@ pub fn HomePage() -> impl IntoView {
                             <div class="text-side">
                                 <div class="title-text-home-page">"Informacje"</div>
                                 <div class="body-text">"BookUp to aplikacja stworzona z myślą o pasjonatach książek. Nasza platforma umożliwia łatwe dodawanie książek do własnej kolekcji, ich ocenę, pisanie recenzji oraz udostępnianie rekomendacji z przyjaciółmi. Dzięki BookUp możesz odkrywać nowe tytuły i dzielić się swoimi ulubionymi lekturami z innymi."</div>
-                                <button class="button-pop-reverse">"Dowiedź się więcej"</button>
+                                <button class="button-pop-reverse" onclick="window.location.href='about'">"Dowiedź się więcej"</button>
                             </div>
                             
                             <img src="https://static.wixstatic.com/media/117514_947e901d593448e98241dfe06415d791~mv2.png" alt="Description" class="image-side"></img>
@@ -29,15 +33,30 @@ pub fn HomePage() -> impl IntoView {
                         <div class="text-title">"Nasze usługi"</div>
                         <div class="container-flex">
                             <div>
-                              <img ssrc="https://static.wixstatic.com/media/117514_2210c99136a548e882210b1319375561~mv2.png" style="width: auto; height: 295px; object-fit: cover;"></img>
+                              <img src="https://static.wixstatic.com/media/117514_2210c99136a548e882210b1319375561~mv2.png" style="width: auto; height: 260px; object-fit: cover;"></img>
                               <div class="title-text-home-page">"Dodawanie Książek"</div>
                               <div class="body-text">"Dodawaj swoje ulubione książki do personalnej kolekcji w BookUp i miej do nich zawsze łatwy dostęp."</div>
+                            </div>
+                            <div>
+                              <img src="https://static.wixstatic.com/media/117514_37fcd62e9aaf4e66952fcc110b120cb2~mv2.png" style="width: auto; height: 260px; object-fit: cover;"></img>
+                              <div class="title-text-home-page">"Ocenianie i Recenzowanie"</div>
+                              <div class="body-text">"Oceń przeczytane książki, napisz recenzje i podziel się swoją opinią z innymi miłośnikami literatur"</div>
+                            </div>
+                            <div>
+                              <img src="https://static.wixstatic.com/media/117514_4fe1b1c971104a02a1a85ab189c79d55~mv2.png" style="width: auto; height: 260px; object-fit: cover;"></img>
+                              <div class="title-text-home-page">"Funkcje Społecznościowe"</div>
+                              <div class="body-text">"Kreuj społeczność książkową, gdzie pasjonaci literatury mogą wymieniać się rekomendacjami, opiniami i inspiracjami czytelniczymi."</div>
+                            </div>
+                            <div>
+                              <img src="https://images.pexels.com/photos/8450123/pexels-photo-8450123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style="width: auto; height: 260px; object-fit: cover;"></img>
+                              <div class="title-text-home-page">"Filtruj jak chcesz"</div>
+                              <div class="body-text">"Sortuj i fultruj książki tak jak sobie wymarzysz, żeby znaleźć te którę ci odpowiadają."</div>
                             </div>
                         </div>
 
                     </div>
 
-                    <footer>
+                    <footer style="margin-left: 20px;">
                         <p>"© 2025 BookUp"</p>
                     </footer>
                 </main>
