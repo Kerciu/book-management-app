a43bad281b913b8aa89e4748215d9fe8a844498b
Kerciu <kerciuuu@gmail.com>
2025-05-15 12:22:33 +0200

    ensured that user can only post one review about given book

--- backend/review/models.py
+++ backend/review/models.py
@@ -24,6 +24,9 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        unique_together = ["user", "book"]
+
     def __str__(self):
         return f"{self.user.username}'s review of {self.book.title}"
 
