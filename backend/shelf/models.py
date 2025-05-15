36538d5844dc0498a354d4cf740bf13d0c60725c
Julia Czosnek <julia.cz13@o2.pl>
2025-05-15 14:30:14 +0200

    added constraint for deafult shelves

--- backend/shelf/models.py
+++ backend/shelf/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
+from django.db.models import Q, UniqueConstraint
 
 User = get_user_model()
 
@@ -17,11 +18,19 @@ class Shelf(models.Model):
         validators=[MinLengthValidator(5)]
     )
     is_default = models.BooleanField(default=False)
-    shelf_type = models.CharField(max_length=20, choices=SHELF_TYPES, blank=True, null=True)
+    shelf_type = models.CharField(
+        max_length=20, choices=SHELF_TYPES, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name} (by {self.user.username})"
 
-
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'shelf_type'],
+                condition=Q(is_default=True)
+                name='unique_deafult_shelf'
+            )
+        ]
