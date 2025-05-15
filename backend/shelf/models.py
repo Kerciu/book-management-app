2628d7f848ed9cc5315498bd7885d707c0d9f09f
Julia Czosnek <julia.cz13@o2.pl>
2025-05-15 14:25:36 +0200

    updated model and created default shelf types

--- backend/shelf/models.py
+++ backend/shelf/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
 
 User = get_user_model()
 
 
 class Shelf(models.Model):
+    SHELF_TYPES = (
+        ('want_to_read', 'Want to Read'),
+        ('currently_reading', 'Currently Reading'),
+        ('read', 'Read')
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="shelves")
+    name = models.CharField(
+        max_length=30,
+        validators=[MinLengthValidator(5)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    name = models.CharField(min_length=5, max_length=30)
+
+    def __str__(self):
+        return f"{self.name} (by {self.user.username})"
