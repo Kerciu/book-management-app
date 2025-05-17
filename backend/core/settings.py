cf28e4f77a7b516e3fd8e4adae384b9ce3f0f7fb
Kerciu <kerciuuu@gmail.com>
2025-05-17 13:45:05 +0200

    changed renderer class in settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -146,6 +146,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": [
