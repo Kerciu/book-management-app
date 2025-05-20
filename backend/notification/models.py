cdbfb2d496e4a61fb780d17dd7434499b4809f05
Kerciu <kerciuuu@gmail.com>
2025-05-20 17:42:18 +0200

    created basic notification model

--- backend/notification/models.py
+++ backend/notification/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_notifications"
+    )
+    text = models.TimeField()
