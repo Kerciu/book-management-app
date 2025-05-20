2ec8ccac3ef33b6adcd87a62f1cd4f915f3bf095
Kerciu <kerciuuu@gmail.com>
2025-05-20 13:15:03 +0200

    feat: implement like functionality in ReviewViewSet with appropriate permissions

--- backend/review/views.py
+++ backend/review/views.py
@@ -1,3 +1,35 @@
-# from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework import status
 
-# Create your views here.
+from .serializers import (
+    ReviewSerializer,
+    ReviewLikeSerializer,
+)
+
+from .models import ReviewLike
+
+
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    @action(
+        detail=True, methods=["post", "delete"], permission_classes=[IsAuthenticated]
+    )
+    def like(self, request, book_pk=None, pk=None):
+        review = self.get_object()
+
+        like, created = ReviewLike.objects.get_or_create(
+            review=review,
+            user=request.user,
+        )
+
+        if request.method == "DELETE":
+            like.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        serializer = ReviewLikeSerializer(like, context={"request": request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
