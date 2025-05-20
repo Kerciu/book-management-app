96e2ca71d434677446a898d01a87de36f3ef402b
Kerciu <kerciuuu@gmail.com>
2025-05-20 20:03:56 +0200

    refactor: enhance send_notification method to create and send notifications via websocket

--- backend/notification/services.py
+++ backend/notification/services.py
@@ -1,3 +1,6 @@
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 from .models import Notification
 
 
@@ -5,9 +8,25 @@ class NotificationService:
     @classmethod
     def send_notification(cls, user, notification_type, message, payload=None):
 
-        _ = Notification.objects.create(
+        notification = Notification.objects.create(
             user=user,
             notification_type=notification_type,
             message=message,
             payload=payload,
         )
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            f"notifications_{user.id}",
+            {
+                "type": "notify",
+                "notification_id": notification.id,
+                "notification_type": notification_type,
+                "message": message,
+                "payload": payload,
+                "timestamp": notification.created_at.isoformat(),
+                "is_read": False,
+            },
+        )
+
+        return notification
