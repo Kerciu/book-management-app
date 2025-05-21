c4ffc173b250017ce97b6c110d67d3c6e2424c1d
Julia Czosnek <julia.cz13@o2.pl>
2025-05-21 17:21:53 +0200

    added ShelfListView

--- backend/shelf/views.py
+++ backend/shelf/views.py
@@ -13,3 +13,15 @@ class ShelfBaseView(LoginRequiredMixin):
         return self.model.objects.filter(user=self.request.user)
 
 
+class ShelfListView(ShelfBaseView, ListView):
+    template_name = 'shelves/list.html'
+    context_object_name = 'shelves'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        shelves = self.get_queryset()
+        context['default_shelves'] = shelves.filter(is_default=True)
+        context['custom_shelves'] = shelves.filter(is_default=False)
+        return context
+
+
