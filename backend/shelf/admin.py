e2ed431f1d19ab74a92338a9e0a50ba62d043020
Julia Czosnek <julia.cz13@o2.pl>
2025-05-21 17:11:51 +0200

    updated shelf admin

--- backend/shelf/admin.py
+++ backend/shelf/admin.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .models import Shelf
 from django.core.exceptions import ValidationError
 
+
 class ShelfAdminForm(forms.ModelForm):
     class Meta:
         model = Shelf
@@ -25,3 +26,36 @@ class ShelfAdminForm(forms.ModelForm):
                 raise ValidationError("Cannot modify shelf type of existing default shelves")
 
         return cleaned_data
+
+
+@admin.register(Shelf)
+class ShelfAdmin(admin.ModelAdmin):
+    form = ShelfAdminForm
+    list_display = ('name', 'user', 'shelf_type_display', 'is_default', 'created_at')
+    list_filter = ('is_default', 'shelf_type', 'created_at')
+    search_fields = ('name', 'user__username')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'name', 'is_default', 'shelf_type')
+        }),
+        ('Metadata', {
+            'classes': ('collapse',),
+            'fields': ('created_at', 'updated_at'),
+        }),
+    )
+
+    def shelf_type_display(self, obj):
+        return obj.get_shelf_type_display()
+    shelf_type_display.short_description = 'Shelf Type'
+
+    def get_readonly_fields(self, request, obj = None):
+        if obj and obj.is_default:
+            return ['is_default', 'shelf_type'] + self.readonly_fields
+        return self.readonly_fields
+
+    def save_model(self, request, obj, form, change):
+        try:
+            return super().save_model(request, obj, form, change)
+        except ValidationError as e:
+            self.message_user(request, str(e), level=messages.ERROR)
