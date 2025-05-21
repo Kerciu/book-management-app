5181700094e87b17df79f99cecf38550c468f138
Kerciu <kerciuuu@gmail.com>
2025-05-21 19:58:17 +0200

    test: enhance like model tests

--- backend/review/tests/models/test_review_like.py
+++ backend/review/tests/models/test_review_like.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.db import IntegrityError
 
 from authentication.models import CustomUser
 from book.models import Book, Author, Genre, Publisher
+
+from ...models import ReviewLike, Review
 
 
 class ReviewLikeModelTest(TestCase):
@@ -24,7 +27,7 @@ class ReviewLikeModelTest(TestCase):
 
         self.genre = Genre.objects.create(name="Test genre")
 
-        book = Book.objects.create(
+        self.book = Book.objects.create(
             title="Test Book",
             isbn="0987654321",
             published_at=timezone.now().date(),
@@ -32,6 +35,31 @@ class ReviewLikeModelTest(TestCase):
             language="English",
         )
 
-        book.authors.add(self.author)
-        book.genres.add(self.genre)
-        book.publishers.add(self.publisher)
+        self.book.authors.add(self.author)
+        self.book.genres.add(self.genre)
+        self.book.publishers.add(self.publisher)
+
+        self.review = Review.objects.create(
+            user=self.user,
+            book=self.book,
+            rating=5,
+            text="very nice book",
+        )
+
+    def test_create_like(self):
+        like = ReviewLike.objects.create(user=self.user, review=self.review)
+        self.assertEqual(like.user, self.user)
+        self.assertEqual(like.review, self.review)
+        self.assertIsNotNone(like.created_at)
+        self.assertEqual(self.user.liked_reviews.count(), 1)
+        self.assertEqual(self.review.likes.count(), 1)
+
+    def test_unique_like_constraint(self):
+        ReviewLike.objects.create(user=self.user, review=self.review)
+        with self.assertRaises(IntegrityError):
+            ReviewLike.objects.create(user=self.user, review=self.review)
+
+    def test_str_representation(self):
+        like = ReviewLike.objects.create(user=self.user, review=self.review)
+        expected_str = f"{self.user.username} liked {self.review}"
+        self.assertEqual(str(like), expected_str)
