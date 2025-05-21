5c5eb70c97e291e4adb701fe774c543a60c0b41b
Julia Czosnek <julia.cz13@o2.pl>
2025-05-21 17:17:35 +0200

    added shelf based biew

--- backend/shelf/views.py
+++ backend/shelf/views.py
@@ -1,3 +1,15 @@
-# from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import Shelf
 
-# Create your views here.
+
+class ShelfBaseView(LoginRequiredMixin):
+    model = Shelf
+    succes_url = reverse_lazy('shelf-list')
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
