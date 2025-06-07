46633d36a90e8c76cae2862e4675278a4dc61a98
Julia Czosnek <julia.cz13@o2.pl>
2025-06-07 20:01:10 +0200

    added add book and get books in shelf

--- backend/shelf/views.py
+++ backend/shelf/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from .models import Shelf
-from .serializers import ShelfSerializer
+from .serializers import ShelfSerializer,  AddBookToShelfSerializer
+from book.serializers import BookSerializer
 
 
 class ShelfViewSet(viewsets.ModelViewSet):
@@ -21,3 +24,21 @@ class ShelfViewSet(viewsets.ModelViewSet):
         if instance.is_default:
             raise ValidationError("Cannot delete default shelves")
         instance.delete()
+
+    @action(detail=True, methods=["get"])
+    def books(self, request, pk=None):
+        """GET /shelves/{id}/books/ - List books on a shelf"""
+        shelf = self.get_object()
+        books = shelf.books.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["post"])
+    def add_book(self, request, pk=None):
+        """POST /shelves/{id}/add_book/ - Add a book to the shelf"""
+        shelf = self.get_object()
+        serializer = AddBookToShelfSerializer(
+            data=request.data, context={'shelf': shelf})
+        serializer.is_valid(raise_exception=True)
+        shelf.books.add(serializer.validated_data['book_id'])
+        return Response({"detail": "Book added to shelf."}, status=status.HTTP_200_OK)
