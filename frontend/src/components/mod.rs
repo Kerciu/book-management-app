8ed18fcd0ed7ec84f03af6f44d3abd3119fded93
Filip Kopyt <filipkopyt14@gmail.com>
2025-06-14 12:37:15 +0200

    Change request functions implementation slightly

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -3,25 +3,27 @@ mod book_list;
 mod email_resend_form;
 mod email_verify_form;
 mod friends_list;
+mod github_auth;
 mod google_auth;
 mod login_form;
 mod registraction_form;
-mod github_auth;
 mod review;
+mod review_input;
 mod review_list;
+mod shelves_list;
 
-pub use github_auth::{GithubAuthButton, GithubAuthHandler};
 pub use book_details::BookDetails;
 pub use book_list::BookList;
 pub use email_verify_form::EmailVerificationForm;
 pub use friends_list::FriendList;
+pub use github_auth::{GithubAuthButton, GithubAuthHandler};
 pub use login_form::LoginForm;
 pub use registraction_form::RegistractionForm;
 
-use crate::{auth, BACKEND};
+use crate::{BACKEND, auth};
 use gloo_net::http::{Request, Response};
 use leptos::prelude::*;
-use serde::{Deserialize, de::DeserializeOwned};
+use serde::de::DeserializeOwned;
 
 /// Utility function to send POST requests as JSON
 async fn send_post_request(
@@ -30,13 +32,22 @@ async fn send_post_request(
 ) -> anyhow::Result<Response> {
     let endpoint = format!("{BACKEND}{endpoint}");
     let body = serde_json::to_string(&data)?;
-    let response = Request::post(&endpoint)
-        .header("Content-Type", "application/json")
-        .header("Authorization", &format!("Bearer {}", &use_context::<auth::Token>().unwrap_or_default() as &str))
-        .body(body)?
-        .send()
-        .await?;
+    let mut request = Request::post(&endpoint).header("Content-Type", "application/json");
+    if let Some(token) = use_context::<auth::Token>() {
+        request = request.header("Authorization", &format!("Bearer {}", &token as &str))
+    }
+    let response = request.body(body)?.send().await?;
     Ok(response)
+}
+
+async fn send_delete_request(endpoint: &str) -> anyhow::Result<()> {
+    let endpoint = format!("{BACKEND}{endpoint}");
+    let mut request = Request::delete(&endpoint).header("Content-Type", "application/json");
+    if let Some(token) = use_context::<auth::Token>() {
+        request = request.header("Authorization", &format!("Bearer {}", &token as &str))
+    }
+    let _ = request.send().await?;
+    Ok(())
 }
 
 async fn send_get_request<'a, T>(endpoint: &str) -> anyhow::Result<T>
@@ -44,11 +55,11 @@ where
     T: DeserializeOwned,
 {
     let endpoint = format!("{BACKEND}{endpoint}");
-    let response = Request::get(&endpoint)
-        .header("Content-Type", "application/json")
-        .header("Authorization", &format!("Bearer {}", &use_context::<auth::Token>().unwrap_or_default() as &str))
-        .send()
-        .await?;
+    let mut request = Request::get(&endpoint).header("Content-Type", "application/json");
+    if let Some(token) = use_context::<auth::Token>() {
+        request = request.header("Authorization", &format!("Bearer {}", &token as &str))
+    }
+    let response = request.send().await?;
     Ok(response.json().await?)
 }
 
