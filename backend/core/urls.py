d7ce5eb2d9502f4c8fae260d52e4bb030ec65772
Julia Czosnek <julia.cz13@o2.pl>
2025-06-19 14:31:21 +0200

    hopefully solved merge conflicts

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("api/book/", include("book.urls")),
     path("api/shelf/", include("shelf.urls")),
     path("api/social/", include("social.urls")),
+    path("api/review/", include("review.urls")),
     # Docs
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(), name="schema-redoc"),
