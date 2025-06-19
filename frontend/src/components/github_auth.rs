b82527f73cade269070871eac463dcce2957726c
Filip Kopyt <filipkopyt14@gmail.com>
2025-06-19 23:43:48 +0200

    Crunching time

--- frontend/src/components/github_auth.rs
+++ frontend/src/components/github_auth.rs
@@ -31,6 +31,12 @@ pub fn github_auth_button() -> impl IntoView {
 
 async fn post(code: String) -> anyhow::Result<AuthResponse> {
     provide_context::<Option<Token>>(None);
+    let _ = web_sys::window()
+        .unwrap()
+        .local_storage()
+        .unwrap()
+        .unwrap()
+        .remove_item("access_token");
     let res = send_post_request(AuthRequest { code }, "/api/auth/github-auth/")
         .await?;
     if !res.ok() {
@@ -67,7 +73,7 @@ pub fn github_auth_handler() -> impl IntoView {
     let code = LocalResource::new(move || post(code().unwrap_or_default()));
     Effect::new(move || if let Some(Ok(AuthResponse { code })) = &*code.read() {
         provide_context(Some(auth::github::Token::new(code.clone())));
-        use_navigate()("/books/list", Default::default());
+        use_navigate()("/main", Default::default());
     });
     Effect::new(move || log::log!(Level::Debug, "{:?}", use_context::<auth::github::Token>()));
 
